588d9f179c000923a557e4df0a14336d81ad1e21
Jefferson Amstutz <jamstutz@nvidia.com>
2021-11-02 08:44:30 -0500

    fix warning of repeated C handle typedef

--- libs/anari/include/anari/anari.h
+++ libs/anari/include/anari/anari.h
@@ -114,7 +114,7 @@ typedef void _ANARIObject;
    ANARIObject, etc. */
 typedef _ANARIObject *ANARIObject, *ANARILibrary, *ANARIDevice, *ANARICamera,
     *ANARIData, *ANARIArray, *ANARIArray1D, *ANARIArray2D, *ANARIArray3D,
-    *ANARIFrame, *ANARIGeometry, *ANARIGroup, *ANARIGroup, *ANARIInstance,
+    *ANARIFrame, *ANARIGeometry, *ANARIGroup, *ANARIInstance,
     *ANARILight, *ANARIMaterial, *ANARIObject, *ANARIRenderer, *ANARISampler,
     *ANARISurface, *ANARISpatialField, *ANARIVolume, *ANARIWorld;
 #endif
