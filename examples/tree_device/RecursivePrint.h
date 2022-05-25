451273be7bdd53762f49d0bb5982baa6562ee87f
Jakob Progsch <jprogsch@nvidia.com>
2022-05-25 21:47:43 +0200

    add tree device example demonstrating the frontend generator

---
+++ examples/tree_device/RecursivePrint.h
@@ -0,0 +1,78 @@
+#include "TreeDevice.h"
+#include "anari/type_utility.h"
+
+#include <iostream>
+
+namespace anari {
+namespace tree {
+
+template<int T>
+struct param_printer {
+    void operator()(ParameterBase &param) {
+        typename ANARITypeProperties<T>::array_type data;
+        param.get(T, data);
+        int components = ANARITypeProperties<T>::components;
+        std::cout << ANARITypeProperties<T>::enum_name << ' ';
+        if(components>1) {
+            std::cout << '(';
+            std::cout << data[0];
+            for(int i = 1;i<components;++i) {
+                std::cout << ", " << data[i];
+            }
+            std::cout << ')';
+        } else {
+            std::cout << data[0];
+        }
+    }
+};
+
+static void printParam(ParameterBase &param) {
+    if(param) {
+        anariTypeInvoke<void, param_printer>(param.type(), param);
+    } else {
+        std::cout << "nil";
+    }
+}
+
+static void recursivePrint(TreeDevice *d, ANARIObject obj, int depth = 0) {
+    ObjectBase *o = d->fromHandle<ObjectBase*>(obj);
+
+    // get some parameter info
+    auto &params = o->parameters();
+    size_t count = params.paramCount();
+    const char ** paramnames = params.paramNames();
+
+    // print object name
+    for(int j = 0;j<depth;++j) { std::cout << "   "; }
+    std::cout << toString(o->type()) << ' ';
+    if(o->subtype()) {
+        std::cout << o->subtype() << ' ';
+    }
+    std::cout << obj << '\n';
+
+    // iterate over parameters
+    for(size_t i = 0;i<count;++i) {
+        auto &param = params[i];
+        if(param) {
+            for(int j = 0;j<depth;++j) { std::cout << "   "; }
+            std::cout << "- " << paramnames[i] << ' ';
+            printParam(param);
+            std::cout << '\n';
+        }
+        // the parameter holds a h andle descend into that object
+        if(isObject(param.type())) {
+            recursivePrint(d, param.getHandle(), depth + 1);
+        }
+    }
+
+    // if this is an array holding objects descend into those too
+    if(Object<Array1D> *array = dynamic_cast<Object<Array1D>*>(o)) {
+        auto &objects = array->objects();
+        for(auto handle : objects) {
+            recursivePrint(d, handle, depth + 1);
+        }
+    }
+}
+
+}
+}
