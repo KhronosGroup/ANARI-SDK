676577a754770862ef34596ffd166d7b1c8160d5
Jefferson Amstutz <jamstutz@nvidia.com>
2022-07-15 12:25:35 -0500

    fix region types to include dimensions

--- libs/debug_device/ExtendedQueries.cpp
+++ libs/debug_device/ExtendedQueries.cpp
@@ -10463,7 +10463,7 @@ static const void * ANARI_ARRAY1D_info(int infoName, ANARIDataType infoType) {
          if(infoType == ANARI_PARAMETER_LIST) {
             static const ANARIParameter parameters[] = {
                {"name", ANARI_STRING},
-               {"region", ANARI_UINT64_REGION},
+               {"region", ANARI_UINT64_REGION1},
                {0, ANARI_UNKNOWN}
             };
             return parameters;
