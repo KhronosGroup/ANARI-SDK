9a1b1af3ce35f81ea47cdae3e5e6929c9115aa46
Patrick Härtl <haertl@ux3d.io>
2022-10-12 11:11:11 +0200

    Add dummy code for test scenes

--- cts/cts.py
+++ cts/cts.py
@@ -4,6 +4,8 @@ import argparse
 import threading
 from pathlib import Path
 from tabulate import tabulate
+from PIL import Image
+import json
 
 logger_mutex = threading.Lock()
 
@@ -16,11 +18,55 @@ def anari_logger(message):
 def check_core_extensions(anari_library, anari_device = None):
     return ctsBackend.check_core_extensions(anari_library, anari_device, anari_logger)
 
-def render_scenes(anari_library, anari_device = None, anari_renderer = "default", test_scenes = "all", output = "."):
+def resolve_scenes(test_scenes):
     collected_scenes = []
+    if isinstance(test_scenes, list):
+        for test_scene in test_scenes:            
+            path = Path(test_scene)
+            if path.exists():
+                collected_scenes.append(path)
+            else:
+                print(f'Path does not exist: {str(path)}')
+    else:
+        path = Path(test_scenes)
+        if not path.is_dir():
+            print("No valid category")
+            return []
+        collected_scenes = list(path.rglob("*.json"))
+    return collected_scenes
+
+def render_scenes(anari_library, anari_device = None, anari_renderer = "default", test_scenes = "test_scenes", output = "."):
+    
+    collected_scenes = resolve_scenes(test_scenes)
+    if collected_scenes == []:
+        print("No scenes selected")
+        return
+
+    HEIGHT = 1024
+    WIDTH = 1024
+
+    for json_file in collected_scenes:
+        with open(json_file, 'r') as f:
+            parsed_json = json.load(f)
+            ctsBackend.initScene()
+            for [key, value] in parsed_json:
+                if isinstance(value, dict):
+                    if key == "permutations":
+                        #TODO
+                        ctsBackend.setPermutations(value)
+                else:
+                    ctsBackend.setParameter(key, value);
+
+            ctsBackend.commitScene()
+            image_data = ctsBackend.renderScene(HEIGHT, WIDTH)
+            image_out = Image.new("RGBA", (HEIGHT, WIDTH))
+            image_out.putdata(image_data)
+
+
     # TODO: collect scenes
     image_data = ctsBackend.render_scenes(anari_library, anari_device, anari_renderer, collected_scenes, anari_logger)
     # TODO: write to file
+
 
 
 if __name__ == "__main__":
