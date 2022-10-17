ad5c02c7ae19e92fa16eace43016f1d08e28a9dd
Patrick Härtl <haertl@ux3d.io>
2022-10-17 10:44:08 +0200

    Add clean metaData step

--- cts/createReferenceData.py
+++ cts/createReferenceData.py
@@ -11,10 +11,20 @@ def writeMetaData(sceneGenerator, scene_location, permutationString):
     frame_duration = sceneGenerator.getFrameDuration()
     if frame_duration <= 0 :
         print(f'Frame duration invalid: {frame_duration}')
-    original_json["metaData"] = {}
+    if "metaData" not in original_json:
+        original_json["metaData"] = {}
     original_json["metaData"][permutationString] = metaData
     with open(scene_location, 'w') as scene_file:
         json.dump(original_json, scene_file, indent=2)
+
+def cleanMetaData(_lib, _dev, _ren, scene_location, _):
+    original_json = {}
+    with open(scene_location, 'r') as scene_file:
+        original_json = json.load(scene_file)
+    if "metaData" in original_json:
+        del original_json["metaData"]
+        with open(scene_location, 'w') as scene_file:
+            json.dump(original_json, scene_file, indent=2)
 
 
 def createReferenceData(parsed_json, sceneGenerator, anariRenderer, sceneLocation, permutationString):
@@ -26,5 +36,5 @@ if __name__ == "__main__":
     parser.add_argument("--test_scenes", default="test_scenes")
 
     args = parser.parse_args()
-
+    cts.apply_to_scenes(cleanMetaData, "example", "example", "default", args.test_scenes)
     cts.apply_to_scenes(createReferenceData, "example", "example", "default", args.test_scenes)
