2ec9cd704e7d93733a08ddbad9baf107a4e47805
Jefferson Amstutz <jamstutz@nvidia.com>
2023-05-18 11:31:48 -0500

    install anari_test_scenes target

--- libs/anari_test_scenes/anari_test_scenes.h
+++ libs/anari_test_scenes/anari_test_scenes.h
@@ -14,6 +14,8 @@
 #include <functional>
 #include <vector>
 
+#include "anari_test_scenes_export.h"
+
 namespace anari {
 namespace scenes {
 
@@ -61,45 +63,49 @@ using Bounds = std::array<anari::float3, 2>;
 struct TestScene;
 using SceneHandle = TestScene *;
 
-std::vector<std::string> getAvailableSceneCategories();
-std::vector<std::string> getAvailableSceneNames(const char *category);
+ANARI_TEST_SCENES_INTERFACE std::vector<std::string>
+getAvailableSceneCategories();
+ANARI_TEST_SCENES_INTERFACE std::vector<std::string> getAvailableSceneNames(
+    const char *category);
 
 // Create an instance of a scene
-SceneHandle createScene(
+ANARI_TEST_SCENES_INTERFACE SceneHandle createScene(
     anari::Device d, const char *category, const char *name);
 
 // Set a scene parameter to be applied on the next 'commit()'
-void setParameter(SceneHandle s, const std::string &name, Any val);
+ANARI_TEST_SCENES_INTERFACE void setParameter(
+    SceneHandle s, const std::string &name, Any val);
 
 // Apply all parameters on the scene (almost always regenerates the world)
-void commit(SceneHandle s);
+ANARI_TEST_SCENES_INTERFACE void commit(SceneHandle s);
 
 // Get the ANARI world handle from the underlying scene: do not need to release
-anari::World getWorld(SceneHandle s);
+ANARI_TEST_SCENES_INTERFACE anari::World getWorld(SceneHandle s);
 
 // Get the world-space bounds of the underlying scene
-Bounds getBounds(SceneHandle s);
+ANARI_TEST_SCENES_INTERFACE Bounds getBounds(SceneHandle s);
 
 // Get a list of all parameters which this scene can use
-std::vector<ParameterInfo> getParameters(SceneHandle s);
+ANARI_TEST_SCENES_INTERFACE std::vector<ParameterInfo> getParameters(
+    SceneHandle s);
 
 // Get a list of all the camera positions the scene has embedded in it
-std::vector<Camera> getCameras(SceneHandle s);
+ANARI_TEST_SCENES_INTERFACE std::vector<Camera> getCameras(SceneHandle s);
 
 // Ask if the scene can be animated?
-bool isAnimated(SceneHandle s);
+ANARI_TEST_SCENES_INTERFACE bool isAnimated(SceneHandle s);
 
 // If the scene is animated, change the world to the next frame
-void computeNextFrame(SceneHandle s);
+ANARI_TEST_SCENES_INTERFACE void computeNextFrame(SceneHandle s);
 
 // Free the underlying scene (and all ANARI objects created by it)
-void release(SceneHandle s);
+ANARI_TEST_SCENES_INTERFACE void release(SceneHandle s);
 
 // Scene registration /////////////////////////////////////////////////////////
 
 using SceneConstructorFcn = std::function<TestScene *(anari::Device)>;
 
-void registerScene(const std::string &category,
+ANARI_TEST_SCENES_INTERFACE void registerScene(const std::string &category,
     const std::string &name,
     SceneConstructorFcn ctor);
 
