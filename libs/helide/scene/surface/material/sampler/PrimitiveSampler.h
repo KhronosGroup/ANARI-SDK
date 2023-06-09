fe97585420faf53232f08ec4cd73560d0eacf63d
Jefferson Amstutz <jamstutz@nvidia.com>
2023-06-08 21:33:46 -0500

    implement primitive sampler

---
+++ libs/helide/scene/surface/material/sampler/PrimitiveSampler.h
@@ -0,0 +1,25 @@
+// Copyright 2022 The Khronos Group
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+#include "Sampler.h"
+#include "array/Array1D.h"
+
+namespace helide {
+
+struct PrimitiveSampler : public Sampler
+{
+  PrimitiveSampler(HelideGlobalState *d);
+
+  bool isValid() const override;
+  void commit() override;
+
+  float4 getSample(const Geometry &g, const Ray &r) const override;
+
+ private:
+  helium::IntrusivePtr<Array1D> m_array;
+  uint32_t m_offset{0};
+};
+
+} // namespace helide
