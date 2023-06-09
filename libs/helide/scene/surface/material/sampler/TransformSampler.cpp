0e6884025aa7dfcb0782977c5b354ec3013d97af
Jefferson Amstutz <jamstutz@nvidia.com>
2023-06-08 22:10:13 -0500

    fix transform sampler

--- libs/helide/scene/surface/material/sampler/TransformSampler.cpp
+++ libs/helide/scene/surface/material/sampler/TransformSampler.cpp
@@ -18,16 +18,14 @@ void TransformSampler::commit()
   Sampler::commit();
   m_inAttribute =
       attributeFromString(getParamString("inAttribute", "attribute0"));
-  m_inTransform = getParam<mat4>("inTransform", mat4(linalg::identity));
-  m_outTransform = getParam<mat4>("outTransform", mat4(linalg::identity));
+  m_transform = getParam<mat4>("transform", mat4(linalg::identity));
 }
 
 float4 TransformSampler::getSample(const Geometry &g, const Ray &r) const
 {
   if (m_inAttribute == Attribute::NONE)
     return DEFAULT_ATTRIBUTE_VALUE;
-  auto av = linalg::mul(m_inTransform, g.getAttributeValue(m_inAttribute, r));
-  return linalg::mul(m_outTransform, av);
+  return linalg::mul(m_transform, g.getAttributeValue(m_inAttribute, r));
 }
 
 } // namespace helide
