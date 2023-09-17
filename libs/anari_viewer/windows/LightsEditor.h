9b142e78d24c1ca2af317cd2dbac5bbf01dc8aef
Stefan Zellmann <info@szellmann.de>
2023-09-17 23:28:00 +0200

    Add a light editor widget for HDRI lights

--- libs/anari_viewer/windows/LightsEditor.h
+++ libs/anari_viewer/windows/LightsEditor.h
@@ -7,6 +7,7 @@
 #include <anari/anari_cpp/ext/linalg.h>
 #include <anari/anari_cpp.hpp>
 // std
+#include <string>
 #include <vector>
 
 #include "Window.h"
@@ -19,7 +20,8 @@ struct Light
   {
     DIRECTIONAL,
     POINT,
-    SPOT
+    SPOT,
+    HDRI
   };
   LightType type{DIRECTIONAL};
   float intensity{1.f};
@@ -27,6 +29,8 @@ struct Light
   anari::float3 color{1.f};
   anari::float2 directionalAZEL{0.f, 345.f};
   anari::float3 pointPosition{0.f};
+  std::string hdriRadiance;
+  float scale{1.f};
   std::vector<anari::Light> handles;
 };
 
