7081d80d6b0aa1099f2e21790811502e60706f71
Patrick Härtl <haertl@ux3d.io>
2024-07-15 16:24:05 +0200

    Change texture generator

--- cts/header/ColorPalette.h
+++ cts/header/ColorPalette.h
@@ -1,18 +1,18 @@
 #pragma once
 
 #include <vector>
-#include <glm/gtc/matrix_transform.hpp>
+#include "anari/anari_cpp/ext/linalg.h"
 
 namespace colors {
-extern const std::vector<glm::vec3> palette;
+extern const std::vector<anari::math::float3> palette;
 
-static glm::vec3 getColorFromPalette(size_t index)
+static anari::math::float3 getColorFromPalette(size_t index)
 {
   return palette.at(index % palette.size());
 }
 
-static std::vector<glm::vec3> getColorVectorFromPalette(size_t size) {
-  std::vector<glm::vec3> colors;
+static std::vector<anari::math::float3> getColorVectorFromPalette(size_t size) {
+  std::vector<anari::math::float3> colors;
   for (size_t i = 0; i < size; ++i) {
     colors.push_back(getColorFromPalette(i));
   }
