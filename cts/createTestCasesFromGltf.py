50f97c03e9ae07ff50b5b6ba45fa110ef2dc1f0e
David Labode <labode@ux3d.io>
2024-08-16 17:18:35 +0200

    cleanup gltf loading

--- cts/createTestCasesFromGltf.py
+++ cts/createTestCasesFromGltf.py
@@ -4,44 +4,37 @@ import json
 import argparse
 import os
 
-# TODO add support for non-directories
-def create_test_cases_from_gltf(gltf_path, output_path):
-    print(gltf_path)
-    gltf_scenes = gather_gltf(gltf_path)
+def create_test_cases_from_gltf(gltf_dir, output_path):
+    # gather all paths to gltf files in dir
+    gltf_scenes = gather_gltf(gltf_dir)
     for gltf_scene in gltf_scenes:
+        # create paths
+        absolute_gltf_path = Path(__file__).parent / gltf_dir
+        relative_gltf_path = os.path.relpath(gltf_scene.parent, absolute_gltf_path)
+        # print info
+        print("Creating test case")
+        print(f"    Source glTF: " + str(gltf_scene))
+        print(f"    Test case: " + str(absolute_gltf_path.parent / Path(output_path) / Path(relative_gltf_path) / Path(gltf_scene.stem + '.json')))
+        print("")
+        # create test
         json_data = create_test_json(gltf_scene)
-        relative_path = os.path.relpath(gltf_scene.parent, Path(Path(__file__).parent / gltf_path))
-        print("last_mile: " + relative_path)
-        print("glTF: " + str(gltf_scene))
-        if not os.path.exists(output_path + "/" + relative_path):
-            os.makedirs(output_path + "/" + relative_path)
-        with open(output_path + "/" + relative_path + "/" + gltf_scene.stem + '.json', 'w') as f:
+        # write test
+        output_dir = Path(output_path + "/" + relative_gltf_path)
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+        with open(output_dir / Path(gltf_scene.stem + '.json'), 'w') as f:
             json.dump(json_data, f, indent=2)
 
-def gather_gltf(gltf_scenes):
-    collected_gltf = []
-    if isinstance(gltf_scenes, list):
-        # gltf_scenes is a list of paths
-        for gltf_scene in gltf_scenes:
-            path = Path(gltf_scene)
-            if path.exists():
-                collected_gltf.append(path)
-            else:
-                print(f'Path does not exist: {str(path)}')
+def gather_gltf(gltf_dir):
+    absolute_path = Path(Path(__file__).parent / gltf_dir)
+    if absolute_path.is_dir():
+        gltf_files = list(absolute_path.rglob("*.gltf"))
+        glb_files = list(absolute_path.rglob("*.glb"))
+        collected_gltf = gltf_files + glb_files
+        return collected_gltf
     else:
-        path = Path(Path(__file__).parent / gltf_scenes)
-        if not path.is_dir():
-            # gltf_scenes is a file
-            if path.suffix == '.gltf' | path.suffix == '.glb':
-                return [path]
-            print("No valid category")
-            return []
-        else:
-            # gltf_scenes is a directory
-            gltf_files = list(path.rglob("*.gltf"))
-            glb_files = list(path.rglob("*.glb"))
-            collected_gltf = gltf_files + glb_files
-    return collected_gltf
+        print("glTF path must lead to a directory")
+        return []
 
 def create_test_json(gltf_scene):
     json_data = {
