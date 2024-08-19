17896496183fcc474bd03d1548ad41e23e2682e5
Thomas Arcila <tarcila@nvidia.com>
2024-08-19 08:17:35 -0400

    Introduce HdAnariTokens
    It will be used to store predefined ANARI tokens, for instance, matching attribute and other inputs names.

---
+++ src/hdanari/anariTokens.cpp
@@ -0,0 +1,10 @@
+// Copyright 2024 The Khronos Group
+// SPDX-License-Identifier: Apache-2.0
+
+#include "anariTokens.h"
+
+PXR_NAMESPACE_OPEN_SCOPE
+
+TF_DEFINE_PUBLIC_TOKENS(HdAnariTokens, HDANARI_TOKENS);
+
+PXR_NAMESPACE_CLOSE_SCOPE
