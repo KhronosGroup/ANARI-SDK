e8022cb64e019caf3e0a58e614c6052edf4218c7
Matthias Hellmann <m.hellmann@me.com>
2025-01-14 22:27:00 +0100

    set texture wrapping mode to clamp_to_edge

--- src/anari_viewer/windows/Viewport.cpp
+++ src/anari_viewer/windows/Viewport.cpp
@@ -30,6 +30,8 @@ Viewport::Viewport(anari::Device device, const char *name)
   glBindTexture(GL_TEXTURE_2D, m_framebufferTexture);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
   glTexImage2D(GL_TEXTURE_2D,
       0,
       GL_RGBA8,
