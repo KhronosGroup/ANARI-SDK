6fcff2ae56b92d4fcd1238649c88da9cc43492a4
Jefferson Amstutz <jamstutz@nvidia.com>
2025-02-04 18:10:00 -0600

    update anariViewer UI layout

--- examples/viewer/ui_layout.cpp
+++ examples/viewer/ui_layout.cpp
@@ -9,14 +9,14 @@ Size=1920,1174
 Collapsed=0
 
 [Window][Viewport]
-Pos=551,25
-Size=1369,1174
+Pos=550,25
+Size=1370,1174
 Collapsed=0
 DockId=0x00000002,0
 
 [Window][Lights Editor]
-Pos=0,310
-Size=549,889
+Pos=0,611
+Size=548,588
 Collapsed=0
 DockId=0x00000004,0
 
@@ -27,16 +27,17 @@ Collapsed=0
 
 [Window][Scene]
 Pos=0,25
-Size=549,283
+Size=548,584
 Collapsed=0
 DockId=0x00000003,0
 
 [Docking][Data]
-DockSpace     ID=0x782A6D6B Window=0xDEDC5B90 Pos=0,25 Size=1920,1174 Split=X
-  DockNode    ID=0x00000001 Parent=0x782A6D6B SizeRef=549,1174 Split=Y Selected=0x5098EBE6
-    DockNode  ID=0x00000003 Parent=0x00000001 SizeRef=549,283 Selected=0xE192E354
-    DockNode  ID=0x00000004 Parent=0x00000001 SizeRef=549,889 Selected=0x5098EBE6
-  DockNode    ID=0x00000002 Parent=0x782A6D6B SizeRef=1369,1174 CentralNode=1 Selected=0x13926F0B
+DockSpace     ID=0x782A6D6B Pos=0,25 Size=1920,1174 CentralNode=1
+DockSpace     ID=0x80F5B4C5 Window=0x079D3A04 Pos=0,25 Size=1920,1174 Split=X Selected=0xC450F867
+  DockNode    ID=0x00000001 Parent=0x80F5B4C5 SizeRef=548,1174 Split=Y Selected=0x9BF64AD9
+    DockNode  ID=0x00000003 Parent=0x00000001 SizeRef=548,584 Selected=0xE601B12F
+    DockNode  ID=0x00000004 Parent=0x00000001 SizeRef=548,588 Selected=0x9BF64AD9
+  DockNode    ID=0x00000002 Parent=0x80F5B4C5 SizeRef=1370,1174 CentralNode=1 Selected=0xC450F867
 )layout";
 
 const char *getDefaultUILayout()
